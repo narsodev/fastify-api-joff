dbfc591a6c9495d4694be9e55aff8798ec3d53db
Narciso González Calderón <narsodev@gmail.com>
2024-01-20 18:48:44 +0100

    Add endpoints validation

--- apps/server/src/modules/user/user.router.ts
+++ apps/server/src/modules/user/user.router.ts
@@ -1,4 +1,5 @@
 import { FastifyPluginAsync } from 'fastify'
+import { Type } from '@sinclair/typebox'
 import UserPrismaRepository from './repositories/user.prisma-repository.js'
 import db from '../../db.js'
 import UserService from './user.service.js'
@@ -7,32 +8,106 @@ const userRepository = new UserPrismaRepository(db)
 const userService = new UserService(userRepository)
 
 const usersRouter: FastifyPluginAsync = async (fastify, opts) => {
-  fastify.get('/', async (request, reply) => {
-    const users = await userService.getAll()
+  fastify.get(
+    '/',
+    {
+      schema: {
+        response: {
+          200: Type.Array(
+            Type.Object({
+              id: Type.Number(),
+              name: Type.String(),
+              email: Type.String()
+            })
+          )
+        }
+      }
+    },
+    async (request, reply) => {
+      const users = await userService.getAll()
 
-    reply.send(users)
-  })
+      reply.send(users)
+    }
+  )
 
-  fastify.get('/:id', async (request, reply) => {
-    const user = await userService.getById(Number((request.params as any).id))
+  fastify.get(
+    '/:id',
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        response: {
+          200: Type.Object({
+            id: Type.Number(),
+            name: Type.String(),
+            email: Type.String()
+          })
+          // TODO: 404
+        }
+      }
+    },
+    async (request, reply) => {
+      const user = await userService.getById(Number((request.params as any).id))
 
-    reply.send(user)
-  })
+      reply.send(user)
+    }
+  )
 
-  fastify.post('/', async (request, reply) => {
-    const user = await userService.create(request.body as any)
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: Type.Object({
+          name: Type.String(),
+          email: Type.String()
+        }),
+        response: {
+          201: Type.Object({
+            id: Type.Number(),
+            name: Type.String(),
+            email: Type.String()
+          })
+        }
+      }
+    },
+    async (request, reply) => {
+      const user = await userService.create(request.body as any)
 
-    reply.send(user)
-  })
+      reply.status(201).send(user)
+    }
+  )
 
-  fastify.patch('/:id', async (request, reply) => {
-    const user = await userService.update(
-      Number((request.params as any).id),
-      request.body as any
-    )
+  fastify.patch(
+    '/:id',
+    {
+      schema: {
+        params: Type.Object({
+          id: Type.Number()
+        }),
+        body: Type.Object({
+          name: Type.Optional(Type.String()),
+          email: Type.Optional(Type.String())
+        }),
+        response: {
+          200: Type.Object({
+            id: Type.Number(),
+            name: Type.String(),
+            email: Type.String()
+          })
+          // TODO: 404
+        }
+      }
+    },
+    async (request, reply) => {
+      const user = await userService.update(
+        Number((request.params as any).id),
+        request.body as any
+      )
 
-    reply.send(user)
-  })
+      reply.send(user)
+    }
+  )
 }
 
 export default usersRouter
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,10 +1,11 @@
 import fastify from 'fastify'
 import swagger from '@fastify/swagger'
 import swaggerUi from '@fastify/swagger-ui'
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import usersRouter from './modules/user/user.router.js'
 
 export default class Server {
-  private server = fastify()
+  private server = fastify().withTypeProvider<TypeBoxTypeProvider>()
   private port: number
 
   constructor(port: number) {
